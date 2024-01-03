848f3de30beeb891427b03594defbe8379bd2e31
chuongCode <rchuong@u.rochester.edu>
2024-01-02 23:13:13 -0600

    Fixing structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,15 @@ import { SignInButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import Head from "next/head";
 
-import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime";
-dayjs.extend(relativeTime);
-
-import Image from 'next/image'
-
 import { api } from "~/utils/api";
 import type { RouterOutputs } from "~/utils/api";
+
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import Image from "next/image";
 import { LoadingPage } from "~/components/loading";
+
+dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
   const {user} = useUser();
@@ -18,20 +18,21 @@ const CreatePostWizard = () => {
   if (!user) return null;
 
   return (
-    <div className="flex gap-3">
-      <Image 
-        src={user.profileImageUrl} 
-        alt="Profile Image" 
-        className="w-12 h-12 rounded-full"
+    <div className="flex w-full gap-3">
+      <Image
+        src={user.profileImageUrl}
+        alt="Profile image"
+        className="h-12 w-12 rounded-full"
         width={56}
-        height={56} 
+        height={56}
       />
-      <input placeholder="Tell your story through emojis!" className="grow bg-transparent outline-none"/>
-
+      <input
+        placeholder="Tell your story through emojis!"
+        className="grow bg-transparent outline-none"
+      />
     </div>
   );
-
-}
+};
 
 type PostWithAuthor = RouterOutputs["post"]["getAll"][number];
 
@@ -51,11 +52,13 @@ const PostView = (props: PostWithAuthor) => {
       <div className="flex flex-col">
         {/* Trying to avoid doing strings inside of JSX template directly */}
         <div className="flex text-slate-300 gap-1">
-          <span>{`@${author.username!}`}</span>
+          <span>{`@${author.username}`}</span>
           {/* Day.js API for relative time */}
-          <span>{` · ${dayjs(post.createdAt).fromNow()}`}</span>
+          <span className="font-thin">{` · ${dayjs(
+            post.createdAt
+          ).fromNow()}`}</span>
         </div>
-        <span className="text-xl">{post.content}</span>
+        <span className="text-2xl">{post.content}</span>
       </div>
     </div>
   );
@@ -65,18 +68,13 @@ const PostView = (props: PostWithAuthor) => {
 const Feed = () => {
   const { data, isLoading: postsLoading } = api.post.getAll.useQuery();
 
-  if (postsLoading)
-    return (
-      <div className="flex grow">
-        <LoadingPage />
-      </div>
-    );
+  if (postsLoading) return <LoadingPage />;
 
   if (!data) return <div>Something went wrong</div>;
 
   return (
     <div className="flex grow flex-col overflow-y-scroll">
-      {[...data, ...data, ].map((fullPost) => (
+      {[...data, ...data]?.map((fullPost) => (
         <PostView {...fullPost} key={fullPost.post.id} />
       ))}
     </div>
@@ -87,11 +85,11 @@ const Home: NextPage = () => {
 
   const { isLoaded: userLoaded, isSignedIn } = useUser();
 
-  // Start fetching asap
+  // Start fetching ASAP
   api.post.getAll.useQuery();
 
   // User usually loads faster, so return an empty div if both posts and user are not loaded
-  if (!userLoaded) return <div />
+  if (!userLoaded) return <div />;
 
   return (
     <>
@@ -104,9 +102,9 @@ const Home: NextPage = () => {
         <div className ="w-full md:max-w-2xl bg-slate-800 border-x border-slate-500">
           <div className="flex border-b border-slate-500 p-4">
             {!isSignedIn && (
-            <div className="flex justify-center">
-              <SignInButton />
-            </div>
+              <div className="flex justify-center">
+                <SignInButton />
+              </div>
             )}
             {isSignedIn && <CreatePostWizard />}
           </div>
